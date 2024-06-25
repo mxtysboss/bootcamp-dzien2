1351bae62467935afd12791153b7b80e4d1d9593
mxtysboss <mateusiak9@gmail.com>
2024-06-25 19:39:29 +0200

    added way to list wpisy

--- src/dzien2a_frontend/src/components/Blog.vue
+++ src/dzien2a_frontend/src/components/Blog.vue
@@ -1,5 +1,24 @@
 <template>
 <div>
+<button @click="pobierzWpisy">refresh</button>
 siema blog!
+{{ wpisy }}
 </div>
 </template>
+
+<script>
+import { dzien2a_backend } from 'declarations/dzien2a_backend/index';
+
+export default {
+    data(){
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async pobierzWpisy(){
+            this.wpisy = await dzien2a_backend.odczytaj_wpisy();
+        }
+    },
+}
+</script>
