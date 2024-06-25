ccfcfb468074812eacb01a7d4378c99a28747af6
mxtysboss <mateusiak9@gmail.com>
2024-06-25 19:56:49 +0200

    added way to add wpis on frontend

--- src/dzien2a_frontend/src/components/Blog.vue
+++ src/dzien2a_frontend/src/components/Blog.vue
@@ -1,24 +1,29 @@
 <template>
-<div>
-<button @click="pobierzWpisy">refresh</button>
-siema blog!
-{{ wpisy }}
-</div>
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
 </template>
 
 <script>
 import { dzien2a_backend } from 'declarations/dzien2a_backend/index';
 
 export default {
-    data(){
+    data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
-        async pobierzWpisy(){
+        async dodajWpisy() {
+            await dzien2a_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
             this.wpisy = await dzien2a_backend.odczytaj_wpisy();
         }
     },
+
 }
 </script>
