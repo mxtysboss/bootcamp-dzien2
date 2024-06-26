0573c6d6706bd6f64f4dc06a26163d07d8fa6bc1
mxtysboss <mateusiak9@gmail.com>
2024-06-26 19:30:51 +0200

    updated styles and deleting wpisy

--- src/dzien2a_frontend/src/components/Blog.vue
+++ src/dzien2a_frontend/src/components/Blog.vue
@@ -1,24 +1,25 @@
 <template>
-<div>
-<h3 class="text-blue-600">Wpisy na blogu</h3>
-<div class="w-100 flex flex-row-reverse" >
-        <button @click="pobierzWpisy" class="bg-blue-600 rounded-md text-white p-4">refresh</button>
-</div>
-<div class="grid mx-6 gap-4 my-4">
-        <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
-        <p>{{ wpis }}</p>
-        </div> 
-         </div>
-         <div class="flex justify-center flex-col">
-        <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
-        <button @click="dodajWpisy" class="bg-blue-600 rounded-md text-white p-4">dodaj</button>
+    <div>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
-        </div>  
-       
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
+                <p>{{ wpis }}</p>
+                <button  class="bg-blue-600 rounded text-white p-4" @click="deleteWpis(index)">usun</button>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+        </div>
+    </div>
 </template>
 
 <script>
-import { dzien2a_backend } from 'declarations/dzien2a_backend/index';
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
 
 export default {
     data() {
@@ -29,15 +30,19 @@ export default {
     },
     methods: {
         async dodajWpisy() {
-            await dzien2a_backend.dodaj_wpis(this.nowyBlog);
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            await this.pobierzWpisy();
+        },
+        async deleteWpis(index) {
+            await dzien2_backend.usun_wpis(index);
+            await this.pobierzWpisy();
         },
         async pobierzWpisy() {
-            this.wpisy = await dzien2a_backend.odczytaj_wpisy();
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
     async mounted(){
         this.pobierzWpisy()
     }
-
 }
 </script>
